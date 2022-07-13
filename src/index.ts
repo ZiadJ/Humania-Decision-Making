7871907c1c28fd3aea7e4b7d207d6ae925ac0066
Ziad Jeeroburkhan <ziadjkhan@gmail.com>
2022-07-13 17:31:34 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+app.use(cors({
+    origin: '*'
+}));
 
 app.get("/todos", async (req, res) => {
   const todos = await prisma.todo.findMany({
